c3a4c312f61202d21edd2d4a27fc6e13bb6958fb
Mario Jimenez <118569104+mjimenez818@users.noreply.github.com>
2023-03-22 23:06:35 -0700

    Create Post.js

---
+++ server/models/Post.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  {
+    post_Id: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    date: String,
+    text: String,
+    location: Array,
+    replies: {
+      author: {
+      type: String,
+      required: true,
+      },
+      date: String,
+      reply: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
